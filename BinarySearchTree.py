53d42a01c5535781aef09d7aa250e5f9d4df1f9e
JherimyBernas <jherimys.bernas@gmail.com>
2023-01-15 22:11:45 +0800

    Find Minimun and Maximum

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -1,1 +1,78 @@
-# Start
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            # node already exist
+            return
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+    def in_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        return self.left.find_min()
+
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_max()
+
+
+def build_tree(elements):
+    print("Building tree with these elements:", elements)
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+
+if __name__ == '__main__':
+    my_name = ["J", "H", "E", "R", "I", "M", "Y", "B", "E", "R", "N", "A", "S"]
+    name_tree = build_tree(my_name)
+
+    print("J is in the list? ", name_tree.search("J"))
+    print("Minimum Element: ", name_tree.find_min())
+    print("Maximum Element: ", name_tree.find_max())
